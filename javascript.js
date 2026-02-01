fb4f338e64c343baa2285a57a5c742bf0b99d825
internallyderanged <251782989+internallyderanged@users.noreply.github.com>
2026-01-31 18:35:41 -0800

    Game complete. It worksgit add javascript.js

--- javascript.js
+++ javascript.js
@@ -12,7 +12,6 @@ function getComputerChoice() {
     }
 }
 
-
 function getHumanChoice() {
    let uChoice = window.prompt('What\'s your choice?');
     if (uChoice == 'rock') {
@@ -34,47 +33,47 @@ let winner;
 function playRound(humanChoice, computerChoice) {
     if (humanChoice == 'rock' && computerChoice == 'paper') {
         console.log('You lose! Paper beats rock!');
-        console.log((computerScore + 1) + " " + humanScore);
+        console.log("User: " + humanScore + " Comp: " + (computerScore + 1));
         winner = 'computer';
         return 'computer';
     } else if (humanChoice == 'paper' && computerChoice == 'rock') {
         console.log('You win! Paper beats rock!');
-        console.log(computerScore + " " + (humanScore + 1));
+        console.log("User: " + (humanScore + 1) + " Comp: " + computerScore);
         winner = 'user';
         return 'user';
     } else if (humanChoice == 'rock' && computerChoice == 'scissors') {
         console.log('You win! Rock beats scissors!');
-        console.log(computerScore + " " + (humanScore + 1));
+        console.log("User: " + (humanScore + 1) + " Comp: " + computerScore);
         winner = 'user';
         return 'user';
     } else if (humanChoice == 'scissors' && computerChoice == 'rock') {
         console.log('You lose! Rock beats scissors!');
-        console.log((computerScore + 1) + " " + humanScore);
+        console.log("User: " + humanScore + " Comp: " + (computerScore + 1));
         winner = 'computer';
         return 'computer';
     } else if (humanChoice == 'scissors' && computerChoice == 'paper') {
         console.log('You win! Scissors beats paper!');
-        console.log(computerScore + " " + (humanScore + 1));
+        console.log("User: " + (humanScore + 1) + " Comp: " + computerScore);
         winner = 'user';
         return 'user';
     } else if (humanChoice == 'paper' && computerChoice == 'scissors') {
         console.log('You lose! Scissors beats paper!');
-        console.log((computerScore + 1) + " " + humanScore);
+        console.log("User: " + humanScore + " Comp: " + (computerScore + 1));
         winner = 'computer';
         return 'computer';
     } else if (humanChoice == 'rock' && computerChoice == 'rock') {
         console.log('It is a tie!');
-        console.log(computerScore + " " + humanScore);
+        console.log("User: " + humanScore + " Comp: " + computerScore);
         winner = 'noWinner';
         return 'noWinner';
     } else if (humanChoice == 'paper' && computerChoice == 'paper'){
         console.log('It is a tie!');
-        console.log(computerScore + " " + humanScore);
+        console.log("User: " + humanScore + " Comp: " + computerScore);
         winner = 'noWinner';
         return 'noWinner';
     } else if (humanChoice == 'scissors' && computerChoice == 'scissors') {
         console.log('It is a tie!');
-        console.log(computerScore + " " + humanScore);
+        console.log("User: " + humanScore + " Comp: " + computerScore);
         winner = 'noWinner';
         return 'noWinner';
     }
@@ -91,57 +90,12 @@ function playGame() {
        computerScore, humanScore;
     } 
     }
+    if (humanScore > computerScore) {
+        console.log(`Final Score: ${humanScore} ${computerScore}; User wins!`);
+    } else if (humanScore < computerScore) {
+        console.log(`Final Score: ${humanScore} ${computerScore}; Computer wins!`);
+    }
 }
-    /*
-    playRound(getHumanChoice(), getComputerChoice());
-        if (winner == 'computer') {
-        computerScore++;
-    } else humanScore++;
-    playRound(getHumanChoice(), getComputerChoice());
-        if (winner == 'computer') {
-        computerScore++;
-    } else humanScore++;
-    playRound(getHumanChoice(), getComputerChoice());
-        if (winner == 'computer') {
-        computerScore++;
-    } else humanScore++;
-    /*
-    playRound(getHumanChoice(), getComputerChoice());
-       if (winner == 'computer') {
-        computerScore++;
-    } else humanScore++; 
-}
-*/
-
+    
  
 console.log(playGame());
-
-
-
-
-/* Right now, the game works, but the rounds don't add up. The function
-play game is just calling the playRound each time and the same thing is happening.
-We want each round to be different. The score isn't implementing properly
-and the user only gets to make one choice. 
-So here, our ++ playRound IS adding to the score, but it is not recognizing winner = computer,
-so the human score is going up no matter what. So right now our problems are:
-
-2. Score not being incremented properly -- only for human right now
-
-First, let's look into what is happening here. 
-We have playRound(humanChoice, computerChoice);
-So every time we run playRound, we should get 
-getHumanChoice, and getComputerChoice to run,
-
-*/
-
-/*
-Computer Score:
-Human Score:
-
-0 1
-0 1
-1 1
-
-
-*/
